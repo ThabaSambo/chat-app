182811bb15a91b95ac5e176711e4104d434a7e32
ThabaSambo <153593274+ThabaSambo@users.noreply.github.com>
2023-12-12 09:55:46 +0200

    2

--- Geeks_Chat/app/src/main/java/com/example/geeks_chat/MainActivity.java
+++ Geeks_Chat/app/src/main/java/com/example/geeks_chat/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     UserAdpter adpter;
     FirebaseDatabase database;
     ArrayList<Users>usersArrayList;
+    String nnnn;
 
 
     @Override
